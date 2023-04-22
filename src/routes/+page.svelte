938d6e9d904ee8e1e1150d3d0bf4416a2b83f3e3
Alli <milk@cream.glass>
2023-04-22 16:40:00 -0400

    test navi screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,23 @@
 <script>
     import { Button, Container } from "@svelteuidev/core";
-    import Modal from "../components/Modal.svelte";
-    import { writable } from "svelte/store";
     import BattleChip from "../components/BattleChip.svelte";
-
-    const opened = writable(false);
 </script>
 
-<Modal {opened}>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Modal
->
-
-<Button on:click={() => opened.set(true)}>test button</Button>
-
+<!-- 
 <Container override={{ display: "flex" }}>
     <BattleChip />
     <BattleChip />
     <BattleChip />
+</Container> -->
+
+<Container class="custom-paragraph" override={{ width: "75%" }}>
+    <h3>What is <i>MILK-PET</i> ?</h3>
+    <p>
+        <b>MILK-PET</b> is a site that visually mimics the PET from the MegaMan NT
+        Warrior and Battle Network series. We are a fan site that host fan-favorite
+        assets such as mods for Open Net Battle. We do not host roms or anything
+        illegal.
+    </p>
 </Container>
+
+<div class="test-navi"><img src="mm-testnavi.png" alt="" /></div>
