8bad7eb95608d31091d39480ef416ddc901227cb
Alli <milk@cream.glass>
2023-04-22 11:30:11 -0400

    mobile optimizations

--- src/routes/mods/+page.svelte
+++ src/routes/mods/+page.svelte
@@ -1,24 +1,8 @@
 <script>
     import { Box, Grid, Loader } from "@svelteuidev/core";
-    import { onMount } from "svelte";
     import { writable } from "svelte/store";
     import BattleChip from "../../components/BattleChip.svelte";
     import Modal from "../../components/Modal.svelte";
-    import { modList } from "../../stores";
-
-    // const isLoading = writable(true);
-    // const endpoint = "https://www.keristero.xyz";
-
-    // onMount(async () => {
-    //     if (Object.keys($modList).length === 0) {
-    //         const response = await fetch(`${endpoint}/mod_list`);
-    //         const data = await response.json();
-
-    //         if (data) modList.set(data);
-    //     }
-
-    //     isLoading.set(false);
-    // });
 
     export let data;
 
@@ -32,7 +16,7 @@
     <Modal {opened}>
         <Box css={{ display: "flex" }}>
             <BattleChip mod={$selectedMod} />
-            <div>
+            <div class="battle-chip-modal-info">
                 <div class="battle-chip-modal-title">
                     {$selectedMod?.attachment_data?.original_filename || ""}
                 </div>
@@ -52,7 +36,7 @@
         }}
     >
         {#each Object.keys(modList) as modName}
-            <Grid.Col xs={10} lg={2}>
+            <Grid.Col xs={11} md={3} xl={2} override={{ flex: "0 0 33%" }}>
                 <BattleChip
                     mod={modList[modName]}
                     on:click={() => {
