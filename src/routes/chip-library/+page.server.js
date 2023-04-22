98fc61ba8eb60b26eb4d10316e62add969e2ccf1
Alli <milk@cream.glass>
2023-04-22 15:13:47 -0400

    comment out server file

--- src/routes/chip-library/+page.server.js
+++ src/routes/chip-library/+page.server.js
@@ -1,25 +1,25 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const BattleChip = mongoose.model("Chip");
+// const BattleChip = mongoose.model("Chip");
 
-export const load = async function () {
-    const data = await BattleChip.find({}, { limit: 50 }).select({
-        name: 1,
-        description: 1,
-        id: 1,
-        _id: 0,
-    });
-    // const data = new BattleChip({
-    //     id: "66667",
-    //     name: "test chip",
-    //     description: "test",
-    // });
+// export const load = async function () {
+//     const data = await BattleChip.find({}, { limit: 50 }).select({
+//         name: 1,
+//         description: 1,
+//         id: 1,
+//         _id: 0,
+//     });
+//     // const data = new BattleChip({
+//     //     id: "66667",
+//     //     name: "test chip",
+//     //     description: "test",
+//     // });
 
-    // await data.save();
+//     // await data.save();
 
-    console.log({ ...data });
+//     console.log({ ...data });
 
-    return {
-        chips: JSON.parse(JSON.stringify(data)),
-    };
-};
+//     return {
+//         chips: JSON.parse(JSON.stringify(data)),
+//     };
+// };
