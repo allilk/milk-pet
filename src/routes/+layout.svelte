72f042b21aa989e110bf9cfbd41718ca39c8959b
Alli <milk@cream.glass>
2023-05-10 16:42:44 -0400

    remove partytown

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,17 +10,6 @@
     import LeftSidebarContent from "../components/LeftSidebarContent.svelte";
     import FooterContent from "../components/FooterContent.svelte";
     import MarqueeTextWidget from "svelte-marquee-text-widget";
-
-    import { onMount } from "svelte";
-    import { partytownSnippet } from "@builder.io/partytown/integration";
-
-    // Add the Partytown script to the DOM head
-    let scriptEl;
-    onMount(() => {
-        if (scriptEl) {
-            scriptEl.textContent = partytownSnippet();
-        }
-    });
 </script>
 
 <svelte:head>
@@ -29,17 +18,6 @@
         name="description"
         content="MILK-PET is a fan site that visually mimics the PET from the MegaMan NT Warrior and Battle Network series."
     />
-    <script>
-        // Forward the necessary functions to the web worker layer
-        partytown = {
-            forward: ["dataLayer.push"],
-        };
-    </script>
-
-    <!-- `partytownSnippet` is inserted here -->
-    <!-- `partytownSnippet` is inserted here -->
-    <!-- `partytownSnippet` is inserted here -->
-    <script bind:this={scriptEl}></script>
 </svelte:head>
 <SvelteUIProvider>
     <AppShell
