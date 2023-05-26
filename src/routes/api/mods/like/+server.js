7085294764303554b4d5b6579e4f264a74e069ff
Alli <milk@cream.glass>
2023-05-25 22:59:35 -0400

    restrict like api route to same site

--- src/routes/api/mods/like/+server.js
+++ src/routes/api/mods/like/+server.js
@@ -1,7 +1,12 @@
 import { prisma } from "../../../../hooks.server.js";
+import { json } from "@sveltejs/kit";
 
 export async function POST({ request }) {
     try {
+        // Restrict to only same site.
+        if (process.env.HOST !== request.headers.get("origin"))
+            throw new Error("Bad Host");
+
         const { modId, userId } = await request.json();
 
         const newLike = await prisma.like.create({
@@ -20,23 +25,12 @@ export async function POST({ request }) {
             },
         });
 
-        return new Response(
-            JSON.stringify(
-                {
-                    message: "Liked Mod!",
-                    like: newLike,
-                    mod,
-                },
-                {
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                }
-            )
-        );
+        return json({
+            message: "Liked Mod!",
+            like: newLike,
+            mod,
+        });
     } catch (err) {
-        return new Response(
-            JSON.stringify({ message: err.toString() || "Error liking mod" })
-        );
+        return json({ message: err.toString() || "Error liking mod" });
     }
 }
